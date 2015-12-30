d22562c12eff193697586cabd2d48d52b8ba5bed
Dianyong Zheng <dianyong@fastmail.com>
2015-12-30 16:26:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 yum -y install git gcc make zlib-devel pcre-devel openssl-devel readline-devel
 
 if [[ ! -d /tmp/nginx-installer ]]; then
-    git clone https://github.com/xifeng/nginx-installer.git /tmp/nginx-installer
+    git clone https://github.com/dianyong/nginx-installer.git /tmp/nginx-installer
     cd /tmp/nginx-installer
 else
     cd /tmp/nginx-installer
